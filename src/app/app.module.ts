e7e2a0321a575f8d25117bda45b6e62685213a84
LuisDio <luis.diolindo@gmail.com>
2019-04-03 23:10:42 +0300

    new order component and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   imports:      [ BrowserModule, CustomersModule, SharedModule, CoreModule, AppRoutingModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, OrdersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
